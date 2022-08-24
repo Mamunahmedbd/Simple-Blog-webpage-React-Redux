c6dc2d630388f2cffcc90438d08ca92ebee16e8c
Mamun Ahmed <mamunahmedusa@gmail.com>
2022-08-24 21:49:09 +0600

    This project added debounce features on the searching

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -6,43 +6,21 @@ import BlogLayout from "./BlogLayout";
 function Posts() {
   const { posts } = useSelector((state) => state.postReducer);
   const filters = useSelector((state) => state.filters);
-  // const filterByPost = (post) => {
-  //   const { value } = filters;
-  //   console.log(value);
-  //   if (value.length > 0) {
-  //     return post.title.toLowerCase().includes(value.toLowerCase().trim());
-  //   }
-  //   return true;
-  // };
 
-  // function debounce(fn, delay) {
-  //   let timeOut
-  //   return function () {
-  //         if (timeOut) {
-  //           clearTimeout(timeOut);
-  //         }
-  //         timeOut = setTimeout(() => {
-  //           fn();
-  //         }, delay);
-  //   }
-  // }
+  let debounceFilter = (post) => {
+    const { value } = filters;
+    console.log(value);
+    if (value.length > 0) {
+      return post.title.toLowerCase().includes(value.toLowerCase().trim());
+    }
+    return true;
+  };
 
   return (
     <BlogLayout>
-      {posts
-        .filter((post) => {
-          const { value } = filters;
-          console.log(value);
-          if (value.length > 0) {
-            return post.title
-              .toLowerCase()
-              .includes(value.toLowerCase().trim());
-          }
-          return true;
-        })
-        .map((post) => (
-          <Post key={post.id} {...post} />
-        ))}
+      {posts.filter(debounceFilter).map((post) => (
+        <Post key={post.id} {...post} />
+      ))}
     </BlogLayout>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,18 +2,24 @@ import React from "react";
 import image from "../image/search.svg";
 import { useDispatch } from "react-redux";
 import { searchValue } from "./Redux/Filters/Action";
-import debounce from "lodash.debounce";
+
 function SearchBar() {
   const dispatch = useDispatch();
 
-  const handleDebounce = (value) => {
-    debounce(dispatch(searchValue(value)), 2000);
-  };
+  function handleBounce(value) {
+    const handler = setTimeout(() => {
+      dispatch(searchValue(value));
+    }, 1400);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }
 
   return (
     <div className="border mt-6 border-slate-200 flex items-center w-11/12 lg:w-1/2 mx-auto bg-gray-50 h-12 px-5 rounded-lg text-sm ring-emerald-200">
       <input
-        onChange={(e) => handleDebounce(e.target.value)}
+        onChange={(e) => handleBounce(e.target.value)}
         className="outline-none border-none bg-gray-50 h-full w-full mr-2"
         type="search"
         name="search"
